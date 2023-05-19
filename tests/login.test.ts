4243b5bea1bfc1569976c68d4fc8f3404c1b49eb
Waliullah Khan <94555999+voliullah@users.noreply.github.com>
2023-05-19 14:28:43 +0500

    here is the new changes

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,21 +1,23 @@
 import {test ,expect ,chromium} from '@playwright/test'
-const baseURL = 'http://54.154.82.253:3001/';
+import { locator } from './data/locator';
+import { testData } from './data/testData';
+
 
 test('Login in to the page with right credentials and see if its being logged in ', async ({ page }) => {
     // Go to the login page
-    await page.goto(baseURL);
+    await page.goto(locator.BaseURL);
   
     // Enter username and password
-    await page.fill('input[type=email]', 'jens@gmail.com');
-    await page.fill('input[type=password]', '123456');
+    await page.fill(locator.email, testData.Login.username);
+    await page.fill(locator.password, testData.Login.password);
   
     // Click the login button
-    await page.click('button[type=submit]');
+    await page.click(locator.loginButton);
   
     // Wait for navigation to complete
     await page.waitForNavigation();
   
     // Check if login was successful
-    const loggedInUsername = await page.innerText('//div[@class="pt-1 col-md-8"]//ul//li');
-    expect(loggedInUsername).toBe('jens');
+    const loggedInUsername = await page.innerText(locator.jensAndSuperAdminText);
+    expect(loggedInUsername).toContain('jens');
 })
